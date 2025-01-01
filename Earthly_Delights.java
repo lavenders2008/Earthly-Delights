8fccf079a3fa64f36fd4f72cc5e2adf463a4ad7c
lavenders2008 <hasinisv7@gmail.com>
2025-01-01 00:16:53 -0600

    nav bar styling

--- Earthly_Delights.java
+++ Earthly_Delights.java
@@ -3,7 +3,7 @@ import java.awt.Desktop;
 
 public class Earthly_Delights{
     public static void main(String[] args)throws Exception {
-    String URL = "Main.html";
+    String URL = "index.html";
     File file =  new File(URL);
     Desktop.getDesktop().browse(file.toURI());   
 
